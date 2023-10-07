5f21f6a0902c547e99f1140d4c002119a500f038
Charles De Dampierre <charles.de-dampierre@hec.edu>
2023-10-07 16:05:49 +0200

    add all

---
+++ bunka-streamlit/app.py
@@ -0,0 +1,46 @@
+from bunkatopics import Bunka
+import pandas as pd
+import streamlit as st
+from langchain.embeddings import HuggingFaceEmbeddings
+import random
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+# Define a Streamlit app
+st.title("Topic Modeling with Bunka")
+
+# Upload CSV file
+csv_file = st.file_uploader("Upload a CSV file", type=["csv"])
+
+gen_ai = False
+
+# from langchain.llms import OpenAI
+# openai_api_key = st.text_input("Enter Your OpenAI API-key")
+# generative_model = OpenAI(openai_api_key=openai_api_key)
+
+
+if csv_file is not None:
+    # Read CSV into a DataFrame
+    df = pd.read_csv(csv_file)
+
+    # Extract text from the CSV column named 'text' (adjust column name as needed)
+    text_data = df["text"].tolist()
+
+    # Sample a subset of the text data (you can adjust the sample size)
+    sample_size = 500
+    full_docs = random.sample(text_data, sample_size)
+
+    embedding_model = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+    bunka = Bunka(embedding_model=embedding_model)
+
+    bunka.fit(full_docs)
+
+    st.subheader("Topic Modeling Visualization")
+    num_clusters = 5
+    df_topics = bunka.get_topics(n_clusters=num_clusters)
+
+    # Visualize topics
+    topic_fig = bunka.visualize_topics(width=800, height=800)
+    st.plotly_chart(topic_fig)
